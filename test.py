70b2b22699028a553fa2627608b65b70c7bec069
Genetical <sejdiaarion@gmail.com>
2018-09-05 19:46:09 +0100

    Removed test.py
    Its, a test, shouldn't even be there.

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-mpath = f"{os.getcwd()}/extensions"
-for file in [f"extensions.{os.path.splitext(f)[0]}" for f in os.listdir(mpath) if os.path.isfile(os.path.join(mpath, f))]:
-    print(file)
