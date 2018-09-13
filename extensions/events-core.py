ad7e95f6b546bf97cd3e8dba6bfaafd1290aefb4
Genetical <creepertheexploding@gmail.com>
2018-09-13 13:58:58 +0100

    Shoud have merged!

--- extensions/events-core.py
+++ extensions/events-core.py
@@ -11,17 +11,18 @@ class events_core:
         self.bot = bot
 
     async def on_message(self, message):
+        print("",end="")
         # Handle points gained for sending messages
         ## Cache last message from user to stop spam
         ## Cooldown of 30 seconds
         ## Add credits
-        self.bot.process_commands(message)
+
 
     async def on_member_join(self, member):
         log.info(f"Member: '{member.display_name}' has joined the channel.")
-        if pf.is_clean(member.name):
-            member.send("Your username has profanity in it and for that reason, you have been kicked, please change your name and rejoin")
-            member.kick(reason="Profanity in username")
+        if not pf.is_clean(member.name):
+            await member.send("Your username has profanity in it and for that reason, you have been kicked, please change your name and rejoin")
+            await member.kick(reason="Profanity in username")
             log.info(f"User {pf.censor(member.name)} was kicked for profanity.")
 
 
