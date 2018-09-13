f1712ffafbdbfbeab266844c8dd60f341cc69e22
Genetical <creepertheexploding@gmail.com>
2018-09-13 13:59:15 +0100

    Shoud have merged!

---
+++ utils/api.py
@@ -0,0 +1,53 @@
+import requests
+client_key="REDACTED"
+
+class account:
+    def __init__(self, zc, items):
+
+        if zc == None:
+            self.zc = 0
+        else:
+            self.zc = zc
+
+        if len(items) == 0:
+            self.items = None
+        else:
+            self.items = items
+
+    def __str__(self):
+        return f"{self.zc}ZC with items: {self.items}"
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class NoAccountException(Error):
+    def __init__(self, id, response):
+        self.id = id
+        self.response = response
+
+class ServerResponseException(Error):
+    def __init__(self, code, response):
+        self.status_code = code
+        self.response = response
+
+
+def lookup(id):
+    URL = f"https://www.zolts.ga/api/check/get_user?client_key={client_key}&id={id}"
+
+    r = requests.get(URL)
+    if (r.headers['Content-Type'] != "application/json") or (r.status_code != 200):
+        raise ServerResponseException(requests.status_codes._codes[r.status_code][0], r.text)
+        return None
+
+    j = r.json()
+    if "ERROR" in j.keys():
+        err = j["ERROR"]
+        if err == "invalid_id":
+            raise NoAccountException(id, err)
+        else:
+            raise ServerResponseException(requests.status_codes._codes[r.status_code][0], err)
+        return None
+
+    finance = j["zolts_account"]["finance"]
+    return account(finance["zc"], finance["items"])
