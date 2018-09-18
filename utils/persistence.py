093a894f22170e628a9980da2d5d5ceef5d2c334
Genetical <sejdiaarion@gmail.com>
2018-09-18 18:59:17 +0100

    added ban function and removed testing code

--- utils/persistence.py
+++ utils/persistence.py
@@ -1,5 +1,9 @@
 import sqlite3
 import pickle
+import sys
+import sqlite3
+sys.path.append("..")
+from utils import timeframe
 #open("persistence.db", "w").close()
 
 conn = sqlite3.connect("persistence.db")
@@ -14,19 +18,22 @@ start_date real NOT NULL,
 end_date real NOT NULL,
 reason text NOT NULL
 )""")
+def ban(moderator, target, length, reason):
+    command = """INSERT INTO bans(moderator, target, start_date, end_date, reason) VALUES (?,?,datetime('now'),datetime(?, 'unixepoch', 'localtime'),?)"""
 
-command = """INSERT INTO bans(moderator, target, start_date, end_date, reason) VALUES (?,?,datetime('now'),datetime(?, 'unixepoch', 'localtime'),?)"""
+    c.execute(command, (
+      pickle.dumps(moderator),
+      pickle.dumps(target),
+      timeframe(length),
+      reason
+      ))
+    conn.commit()
 
-c.execute(command, (
-  pickle.dumps("moderator"), 
-  pickle.dumps("target"),
-  1536842125,
-  "Test reason"
-  ))
-conn.commit()
-
-for item in c.execute("""SELECT * FROM bans""").fetchall():
-      item = dict(zip(list(map(lambda x: x[0], c.description)), item))
-      item["moderator"] = pickle.loads(item["moderator"])
-      item["target"] = pickle.loads(item["target"])
-      
+def fetch_ban():
+    output = []
+    for item in c.execute("""SELECT * FROM bans""").fetchall():
+          item = dict(zip(list(map(lambda x: x[0], c.description)), item))
+          item["moderator"] = pickle.loads(item["moderator"])
+          item["target"] = pickle.loads(item["target"])
+          output.append(item)
+    return output
