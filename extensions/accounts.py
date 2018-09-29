2dc1d4886881a50be693f8dd7513f09bb58cdb38
Genetical <sejdiaarion@gmail.com>
2018-09-29 13:10:08 +0100

    Rework of account system

--- extensions/accounts.py
+++ extensions/accounts.py
@@ -12,11 +12,10 @@ class web_accounts:
     async def account(self,ctx):
         r = utils.api.lookup(ctx.author.id)
         if r.items != None:
-            items = "\n".join(r.items)
         else:
             items = "*No items owned*"
 
-        await ctx.send(f":credit_card: **You have *{round(r.zc, 2)}* ZoltCoins and own the following items:**```\n{items}```")
+        await ctx.send(f":credit_card: ** | You have *{round(r.zc, 2)}* ZoltCoins and own the following items:**```\n{items}```")
 
 def setup(bot):
     bot.add_cog(web_accounts(bot))
