182389af186cbdcdcc914f18eaf2d6e91e26dbec
Genetical <creepertheexploding@gmail.com>
2018-11-01 19:00:11 +0000

    Update utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,5 +3,6 @@ __path__ = pkgutil.extend_path(__path__, __name__)
 for importer, modname, ispkg in pkgutil.walk_packages(path=__path__, prefix=__name__+'.'):
       try:
             __import__(modname)
+            print(f"Module {modname} loaded")
       except Exception as e:
             print(f"Module {modname} failed to load")
