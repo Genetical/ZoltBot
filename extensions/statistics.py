04c87ed285d227025ae6b8cd84f2deac7b55552e
Genetical <creepertheexploding@gmail.com>
2018-11-01 18:49:04 +0000

    Added more to statistics such as cpu usage

--- extensions/statistics.py
+++ extensions/statistics.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 import os
 import asyncio
 import time
+import psutil
 
 
 class statistics:
@@ -14,6 +15,8 @@ class statistics:
         self.bot = bot
         self.command_counter = 0
         self.message_counter = 0
+        self.pid = os.getpid()
+        self.py = psutil.Process(self.pid)
 
     async def stats(self, start_time):
         await asyncio.sleep(5)
@@ -21,7 +24,11 @@ class statistics:
             utils.terminal.clear()
             print(f"Time since execution: {round(time.time() - start_time, 0)} seconds")
             print(f"Messages caught since executed: {self.message_counter}")
-            print(f"Commands ran since execution: {self.command_counter}")
+            print(f"Commands ran since execution: {self.command_counter}",end="\n\n")
+            print("=" * 50)
+            print(f"Memory usage: {round(self.py.memory_info()[0]/2.**30, 2)} GB")
+            print(f"CPU Usage: {psutil.cpu_percent()}%")
+
             await asyncio.sleep(1)
 
     async def on_ready(self):
