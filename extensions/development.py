886241ed8692ed4de5f6cb5c29cfa8e0927fe037
Genetical <sejdiaarion@gmail.com>
2018-11-10 09:24:47 +0000

    Fixed security flaw in localise

--- extensions/development.py
+++ extensions/development.py
@@ -18,6 +18,15 @@ empty = u'\u200b'
 async def is_owner(ctx):
     return ctx.author.id in (173494216263991296, 303953507889840129)
 
+async def localise_perm(ctx):
+    check = False
+    for role in ctx.author.roles:
+        if role.permissions.manage_guild:
+            check = True
+            break
+    check = is_owner(ctx)
+    return True
+
 localise = utils.localisation.localise
 class development:
     def __init__(self, bot):
@@ -37,7 +46,15 @@ class development:
         quit()
 
     @commands.command(pass_context=True)
+    @commands.check(localise_perm)
     async def localise(self, ctx, operation, key=None, value=None):
+        """Edits or views localisation data
+
+        [p]**localise** update [key] [value]: Updates the specific localisation string
+        [p]**localise** read [key]: Will give the stored localisation string for that key
+        [p]**localise** list: Will give a list of all localisation
+        [p]**localise** reset: Will reset localisation to default
+        """
         if operation == "update":
             temp = await ctx.send(f"<a:loading:495280632067522600> **|** Loading...Finding key {key}")
             old_value = utils.localisation.read(key)
@@ -54,16 +71,23 @@ class development:
 
         elif operation == "read":
             temp = await ctx.send(f"<a:loading:495280632067522600> **|** Loading...Finding key {key}")
+
+
             old_value = utils.localisation.read(key)
+
 
             if old_value == None:
                 await temp.edit(content=f"<:xmark:495282541347995667> **|** Key {key} doesn't exist")
                 return
 
-            meta = utils.localisation.meta[key]
+            try:
+                meta = utils.localisation.meta[key]
+            except KeyError:
+                meta = []
+
             clean_meta = '\n'.join(meta)
             clean_value = old_value.replace("`","'")
-            await temp.edit(content=f"Key: {key} has value: \n```{clean_value}```\n This local string has {len(meta)} variable(s) attached:\n*{clean_meta}*")
+            await temp.edit(content=f"Key: `{key}` has value:\n```{clean_value}```\n This local string has {len(meta)} variable(s) attached:\n{clean_meta}")
 
         elif operation == "reset":
             temp = await ctx.send(f"<a:loading:495280632067522600> **|** Resetting localisation")
